c3605d956bc7139084577999815360eb360273cb
shubaivan <shuba.ivan@yandex.ru>
2015-03-31 14:06:10 +0300

    hello hwio

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony.hwio
+============
+
+A Symfony project created on March 31, 2015, 1:24 pm.
---
+++ src/UserBundle/Entity/OAuthUserProvider.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider as BaseOAuthUserProvider;
+
+class OAuthUserProvider extends BaseOAuthUserProvider { }
